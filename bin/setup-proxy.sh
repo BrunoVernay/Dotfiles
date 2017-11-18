ebcd0eb34de5838f0f7f08e89c0e579cdfc7f835
BrunoVernay <BrunoVern.a@gmail.com>
2017-11-18 17:15:55 +0100

    Uses /etc/environment

--- bin/setup-proxy.sh
+++ bin/setup-proxy.sh
@@ -5,8 +5,8 @@ tee /etc/profile.d/proxymaybe.sh <<'EOF'
 
 if ping -q -c 2 -W 3 gad.schneider-electric.com > /dev/null 2>&1 ; then
     echo "Proxy ON"
-    export http_proxy="http://SESA147313@gateway.zscaler.net:80/"
-    export https_proxy=$http_proxy
+#    export http_proxy="http://SESA147313@gateway.zscaler.net:80/"
+#    export https_proxy=$http_proxy
     export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com,.schneider-electric.com"
 
     export npm_config_proxy=$http_proxy
@@ -24,13 +24,27 @@ if ping -q -c 2 -W 3 gad.schneider-electric.com > /dev/null 2>&1 ; then
     gsettings set org.gnome.system.proxy.http authentication-user 'SESA147313'
     gsettings set org.gnome.system.proxy.https host 'gateway.zscaler.net'
     gsettings set org.gnome.system.proxy.https port 443
-    gsettings set org.gnome.system.proxy ignore-hosts ['localhost', '127.0.0.0/8', '::1', '.schneider-electric.com']
+    gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8', '::1', '.schneider-electric.com']"
 else
     echo "Proxy OFF"
     unset -v http_proxy https_proxy no_proxy npm_config_proxy npm_config_https_proxy
     gsettings set org.gnome.system.proxy mode 'none'
 fi
 
+EOF
+
+mkdir -p /opt/bin
+
+tee /opt/bin/proxymaybe.sh <<'EOF'
+#!/bin/bash
+
+sed -i '/proxy/d' /etc/environment
+if [[ $(curl -s -o /dev/null -I -w "%{http_code}" 74.125.133.115) != 301 ]]; then
+ echo 'http_proxy="http://SESA147313@gateway.zscaler.net:80/"' >> /etc/environment
+ echo 'https_proxy="http://SESA147313@gateway.zscaler.net:80/"' >> /etc/environment
+ echo 'no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com,.schneider-electric.com"' >> /etc/environment
+fi
+# if ping -q -c 2 -W 3 gad.schneider-electric.com > /dev/null 2>&1 ; then
 EOF
 
 chmod +x /opt/bin/proxymaybe.sh
@@ -42,7 +56,7 @@ After=network.target
 
 [Service]
 Type=oneshot
-ExecStart=/etc/profile.d/proxymaybe.sh
+ExecStart=/opt/bin/proxymaybe.sh
 
 [Install]
 WantedBy=multi-user.target
